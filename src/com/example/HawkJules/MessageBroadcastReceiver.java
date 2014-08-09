87e5b346e28fea0c0e74aa4d51038ecc1bcda709
juliejk <juliejk92@gmail.com>
2014-08-09 15:37:48 +0200

    refactor

--- src/com/example/HawkJules/MessageBroadcastReceiver.java
+++ src/com/example/HawkJules/MessageBroadcastReceiver.java
@@ -15,13 +15,13 @@ public class MessageBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "received");
         Bundle bundle = intent.getExtras();
-        Log.d(TAG,"my action: " + bundle.getString("my_action"));
+        Log.d(TAG, "my action: " + bundle.getString("my_action"));
         //hei julie jeg elsker deg skikkelig mye og tenker på deg hele tiden mens jeg sitter her oppe uten deg.
         //Jeg vil at du skal vite hvor mye du betyr for meg og at bare med din tilstedeværelse løfter livet
         //mitt. Du er en nusselig liten kosepus! gleder meg til å ha deg her igjen! Forresten, det er denne klassen
         //som mottar det vi sender med knappen vår. Good luck.
         //Jeg elsker deg så utrolig mye. Tenker på deg hvert minutt av hver time hver dag. Det er vondt å være uten deg,
         //og jeg gleder så mye til å hoppe i armene dine igjen. Du er den beste kjæresten! Elsker deg!
-        //Julie elsker Håkon... åååå funk da!!
-        //kom igjen da!
+        //Julie elsker Håkon
+    }
 }
