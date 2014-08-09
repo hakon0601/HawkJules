b7ea15806987d93cea5ccb97377e2ad06e96d1e6
juliejk <juliejk92@gmail.com>
2014-08-09 14:30:17 +0200

    testing

--- src/com/example/HawkJules/MessageBroadcastReceiver.java
+++ src/com/example/HawkJules/MessageBroadcastReceiver.java
@@ -22,5 +22,6 @@ public class MessageBroadcastReceiver extends BroadcastReceiver {
         //som mottar det vi sender med knappen vår. Good luck.
         //Jeg elsker deg så utrolig mye. Tenker på deg hvert minutt av hver time hver dag. Det er vondt å være uten deg,
         //og jeg gleder så mye til å hoppe i armene dine igjen. Du er den beste kjæresten! Elsker deg!
+        //test
     }
 }
